2d9b434acec262b3bdd05fb2c74ca108062f7c7e
Rob Taglang <rob@privatemachines.com>
2018-10-02 15:23:57 -0400

    [intelx] Added PCI_ROM entry for Intel x553/x557-AT and x553 (SFP+) NICs

--- src/drivers/net/intelx.c
+++ src/drivers/net/intelx.c
@@ -802,6 +802,8 @@ static struct pci_device_id intelx_nics[] = {
 	PCI_ROM ( 0x8086, 0x1560, "x540t1", "X540-AT2/X540-BT2 (with single port NVM)", 0 ),
 	PCI_ROM ( 0x8086, 0x1563, "x550t2", "X550-T2", 0 ),
 	PCI_ROM ( 0x8086, 0x15e5, "x553", "X553", INTELX_X550EM_A | INTELX_SW_LINK_UP ),
+	PCI_ROM ( 0x8086, 0x15c8, "x553t", "X553/X557-AT", 0),
+	PCI_ROM ( 0x8086, 0x15ce, "x553-sfp", "X553 (SFP+)", 0),
 };
 
 /** PCI driver */
